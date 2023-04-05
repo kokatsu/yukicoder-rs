59dbbbccc1d5566fa521f78fa1bb7c72710ed0fc
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-04-06 00:13:35 +0900

    solve no0111

---
+++ no0101_0150/src/no0111.rs
@@ -0,0 +1,29 @@
+fn main() {
+    let l: i64 = read();
+
+    let h: i64 = l / 2;
+
+    let res: i64 = h.pow(2);
+    println!("{}", res);
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
