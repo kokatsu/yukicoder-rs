41df40dc8a277e76c21e883ec20eedf3ee8f6abd
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-04-07 10:44:59 +0900

    solve no0182

---
+++ no0151_0200/src/no0182.rs
@@ -0,0 +1,58 @@
+fn main() {
+    let _n: i64 = read();
+    let mut a: Vec<i64> = read_vec();
+
+    a.sort();
+
+    let g: Vec<(i64, i64)> = run_length_encoding(a);
+
+    let mut res: i64 = 0;
+    for (_, b) in g {
+        if b == 1 {
+            res += 1;
+        }
+    }
+
+    println!("{}", res);
+}
+
+#[allow(dead_code)]
+fn run_length_encoding<T: std::cmp::PartialEq + Copy>(x: Vec<T>) -> Vec<(T, i64)> {
+    let mut ret: Vec<(T, i64)> = Vec::new();
+
+    let l: usize = x.len();
+
+    let mut i: usize = 0;
+    let mut j: usize = 0;
+    while i < l {
+        while j < l && x[i] == x[j] {
+            j += 1usize;
+        }
+
+        ret.push((x[i], (j-i) as i64));
+        i = j;
+    }
+
+    ret
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
